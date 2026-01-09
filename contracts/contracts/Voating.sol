088a5504d95cb2e24f62df1d1e27f09c1e1e8112
Manish Dait <daitmanish88@gmail.com>
2026-01-09 15:02:10 +0530

    created voating contract

---
+++ contracts/contracts/Voating.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+error NotAdmin();
+error ElectionNotExists();
+error ElectionNotActive();
+error ElectionAlreadyEnded();
+error ElectionNotEnded();
+error VoterNotRegistered();
+error AlreadyVoted();
+error InvalidCandidate();
+error CandidatesLocked();
+
+contract Voating {
+    event ElectionCreated(uint256 indexed electionId, string name);
+    event CandidateAdded(uint256 indexed electionId, uint256 candidateId, string name);
+    event VoterRegister(uint256 indexed electionId, address voter);
+    event VoteCast(uint256 indexed electionId, address indexed voter);
+    event ElectionEnded(uint256 indexed electionId);
+
+    struct Election {
+        string name;
+        bool isActive;
+        bool isEnded;
+    }
+
+    struct Candidate {
+        string name;
+        uint256 count;
+    }
+
+    address public immutable admin;
+    uint256 public electionCount;
+
+    mapping(uint256 => Election) private elections;
+    mapping(uint256 => Candidate[]) private candidates;
+    mapping(uint256 => mapping(address => bool)) private registerVoters;
+    mapping(uint256 => mapping(address => bool)) private hasVoted;
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) revert NotAdmin();
+        _;
+    }
+
+    modifier electionExists(uint256 electionId) {
+        if (electionId >= electionCount) revert ElectionNotExists();
+        _;
+    }
+
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function createElection(string calldata electionName) external onlyAdmin {
+        elections[electionCount] = Election({
+            name: electionName, 
+            isActive: true, 
+            isEnded: false
+        });
+
+        emit ElectionCreated(electionCount, electionName);
+        electionCount++;
+    }
+
+    function addCandidates(uint256 electionId, string calldata candidateName) external onlyAdmin electionExists(electionId) {
+        Election memory election = elections[electionId];
+
+        if (!election.isActive || election.isEnded) revert CandidatesLocked();
+
+        candidates[electionId].push(
+            Candidate({name: candidateName, count: 0})
+        );
+
+        emit CandidateAdded(
+            electionId, 
+            candidates[electionId].length - 1, 
+            candidateName
+        );
+    }
+
+    function registerVoter(uint256 electionId, address voter) external onlyAdmin electionExists(electionId) {
+        registerVoters[electionId][voter] = true;
+        emit VoterRegister(electionId, voter);
+    }
+
+    function endElection(uint256 electionId) external onlyAdmin electionExists(electionId) {
+        Election storage election = elections[electionId];
+
+        if (election.isEnded) revert ElectionAlreadyEnded();
+
+        election.isActive = false;
+        election.isEnded = true;
+
+        emit ElectionEnded(electionId);
+    }
+
+    function vote(uint256 electionId, uint256 candidateId) external electionExists(electionId) {
+        Election storage election = elections[electionId];
+
+        if (!election.isActive || election.isEnded) revert ElectionNotActive();
+        if (!registerVoters[electionId][msg.sender]) revert VoterNotRegistered();
+        if (hasVoted[electionId][msg.sender]) revert AlreadyVoted();
+        if (candidateId >= candidates[electionId].length) revert InvalidCandidate();
+        
+        hasVoted[electionId][msg.sender] = true;
+        candidates[electionId][candidateId].count++;
+
+        emit VoteCast(electionId, msg.sender);
+    }
+
+    function getElection(uint256 electionId) external view electionExists(electionId) returns (string memory name, bool isActive, bool isEnded) {
+        Election storage election = elections[electionId];
+        return (election.name, election.isActive, election.isEnded);    
+    }
+
+    function getCandidates(uint256 electionId) external view electionExists(electionId) returns (Candidate[] memory) {
+       return candidates[electionId];
+    }
+
+    function hasUserVoted(uint256 electionId, address voter) external view electionExists(electionId) returns (bool) {
+        return hasVoted[electionId][voter];
+    }
+
+    function isVoterRegister(uint256 electionId, address voter) external view electionExists(electionId) returns (bool) {
+        return registerVoters[electionId][voter];
+    }
+}
---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,17 @@
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    version: "0.8.28",
+  },
+  networks: {
+    hardhatLocal: {
+      type: 'http',
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+      chainType: 'generic'
+    }
+  }
+});
